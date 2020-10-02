1e77f0949fc2cba9da9cf765fec9f9d17d7bc294
Alari Jurtom <alarijurtom@gmail.com>
2020-10-02 10:14:44 +0300

    Updates

---
+++ precache-manifest.efc88b536836ecd53b3d657078e27c09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9deda4231e04dc2c1e2903c76fdca2f2",
+    "url": "/myvid/index.html"
+  },
+  {
+    "revision": "25faf8283bc55a2890c7",
+    "url": "/myvid/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "a828a659c39dd2974fe8",
+    "url": "/myvid/static/js/2.95fab56e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myvid/static/js/2.95fab56e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25faf8283bc55a2890c7",
+    "url": "/myvid/static/js/main.1ba11854.chunk.js"
+  },
+  {
+    "revision": "2e6ca4e0a6220fc50437",
+    "url": "/myvid/static/js/runtime-main.dafe2177.js"
+  }
+]);
